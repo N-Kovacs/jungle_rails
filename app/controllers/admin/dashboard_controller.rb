270dd4c628a0de0e4fedb01d7343862f65ed2454
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-11-16 21:24:00 +0000

    dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
+  
+  
   def show
+    @productcount = Product.count
+    @catagories = Category.count
   end
 end
